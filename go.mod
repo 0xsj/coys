bc362f1175da43ba74b1a26399810fda0b64eca3
sjtommylee <sjtommylee@gmail.com>
2023-03-31 14:55:05 -0700

    adapters/FindOne

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/sjtommylee/go-dynamodb
 go 1.20
 
 require (
-	github.com/aws/aws-sdk-go v1.44.233 // indirect
+	github.com/aws/aws-sdk-go v1.44.234 // indirect
 	github.com/go-chi/chi v1.5.4 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -1,6 +1,10 @@
 package adapter
 
-import "github.com/aws/aws-sdk-go/service/dynamodb"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
 
 type Database struct {
 	connection *dynamodb.DynamoDB
@@ -16,7 +20,14 @@ func NewAdapter() Interface {
 
 func (db *Database) FindAll() {}
 
-func (db *Database) FindOne() {}
+func (db *Database) FindOne(condition map[string]interface{}, tablename string) (response *dynamodb.GetItemOutput, err error) {
+	conditionParsed, err := dynamodbattribute.MarshalMap(condition)
+	input := &dynamodb.GetItemInput{
+		TableName: aws.String(tablename),
+		Key:       conditionParsed,
+	}
+	return db.connection.GetItem(input)
+}
 
 func (db *Database) CreateOrUpdate() {}
 
