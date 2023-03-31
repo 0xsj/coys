3e51ec7a76cbf38101a5f59a3b0fefcc0caae39c
sjtommylee <sjtommylee@gmail.com>
2023-03-31 14:30:00 -0700

    chore: commit save point

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,3 +1,35 @@
 package routes
 
-func NewRouter() {}
+import (
+	"github.com/go-chi/chi"
+)
+
+type Router struct {
+	config *Config
+	router *chi.Mux
+}
+
+func NewRouter() *Router {
+	return &Router{
+		config: NewConfig(),
+		router: chi.NewRouter(),
+	}
+}
+
+func (r *Router) SetRouters() *chi.Mux {}
+
+func (r *Router) SetConfigRouter() {}
+
+func RouterHealth() {}
+
+func RouterProduct() {}
+
+func EnableTimeout() {}
+
+func EnableCors() {}
+
+func EnableRecovery() {}
+
+func EnableRequestID() {}
+
+func EnableRealIP() {}
