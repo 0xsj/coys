0aed389b54a6d580ee0e290073c4b728ea6c84c9
sjtommylee <sjtommylee@gmail.com>
2023-03-31 15:22:56 -0700

    response outline

--- utils/http/response.go
+++ utils/http/response.go
@@ -1,1 +1,37 @@
 package http
+
+type Response struct {
+	Status int         `json:"status"`
+	Result interface{} `json:"result"`
+}
+
+func NewResponse() {
+
+}
+
+func Bytes() {}
+
+func String() {}
+
+func SendResponse() {}
+
+// 200
+func StatusOK() {}
+
+// 204
+func StatusNoContent() {}
+
+// 400
+func StatusBadRequest() {}
+
+// 404
+func StatusNotFound() {}
+
+// 405
+func StatusMethodNotAllowed() {}
+
+// 409
+func StatusConflict() {}
+
+// 500
+func StatusInternalServerError() {}
