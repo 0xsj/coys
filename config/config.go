059531375607d181c4e2b3ad920a5d3bf84c6c9f
sjtommylee <sjtommylee@gmail.com>
2023-04-01 15:32:50 -0700

    db config

--- config/config.go
+++ config/config.go
@@ -1,20 +1,31 @@
 package config
 
+import (
+	"strconv"
+
+	"github.com/sjtommylee/go-dynamodb/utils/env"
+)
+
 type Config struct {
 	Port        int
 	Timeout     int
+	Database    string
 	DatabaseURI string
 }
 
 func GetConfig() Config {
 	return Config{
-		Port:        8008,
-		Timeout:     30,
-		DatabaseURI: "",
+		Port:        parseEnvToInt("PORT", "8080"),
+		Timeout:     parseEnvToInt("TIMEOUT", "30"),
+		Database:    env.GetEnv("Database", "sqlite3"),
+		DatabaseURI: env.GetEnv("DATABASE_URI", ":memory"),
 	}
 }
 
 func parseEnvToInt(envName, defaultValue string) int {
-	// num, err := strconv.Atoi(env.Geten)
-	return 0
+	num, err := strconv.Atoi(env.GetEnv(envName, defaultValue))
+	if err != nil {
+		return 0
+	}
+	return num
 }
