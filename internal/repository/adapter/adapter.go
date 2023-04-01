3d4cf03e4587307672350ea2cdd24468a2673ea6
sjtommylee <sjtommylee@gmail.com>
2023-03-31 17:13:57 -0700

    chore: commit save point

--- internal/repository/adapter/adapter.go
+++ internal/repository/adapter/adapter.go
@@ -4,6 +4,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 type Database struct {
@@ -12,13 +13,36 @@ type Database struct {
 }
 
 type Interface interface {
+	Health() bool
+	FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error)
+	FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error)
+	CreateOrUpdate(entity interface{}, tableName string) (response *dynamodb.PutItemOutput, err error)
+	Delete(condition map[string]interface{}, tableName string) (response *dynamodb.DeleteItemOutput, err error)
 }
 
-func NewAdapter() Interface {
-	return nil
+// health check for db, takes in a pointer for struct type db
+func (db *Database) Health() bool {
+	_, err := db.connection.ListTables(&dynamodb.ListTablesInput{}) // discard the first return with _, we only care about the error that it returns
+	return err == nil
 }
 
-func (db *Database) FindAll() {}
+func NewAdapter(conn *dynamodb.DynamoDB) Interface {
+	return &Database{
+		connection: conn,
+		logMode:    false,
+	}
+}
+
+func (db *Database) FindAll(condition expression.Expression, tableName string) (response *dynamodb.ScanOutput, err error) {
+	input := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  condition.Names(),
+		ExpressionAttributeValues: condition.Values(),
+		FilterExpression:          condition.Filter(),
+		ProjectionExpression:      condition.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+	return db.connection.Scan(input)
+}
 
 // method named findone of a struct type
 func (db *Database) FindOne(condition map[string]interface{}, tableName string) (response *dynamodb.GetItemOutput, err error) {
@@ -53,10 +77,4 @@ func (db *Database) Delete(condition map[string]interface{}, tableName string) (
 		TableName: aws.String(tableName),
 	}
 	return db.connection.DeleteItem(input)
-}
-
-// health check for db, takes in a pointer for struct type db
-func (db *Database) Health() bool {
-	_, err := db.connection.ListTables(&dynamodb.ListTablesInput{}) // discard the first return with _, we only care about the error that it returns
-	return err == nil
 }
