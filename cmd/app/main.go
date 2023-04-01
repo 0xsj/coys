a87abdb180dc7167c4b4fc3ffcb7fd087dbe1b6d
sjtommylee <sjtommylee@gmail.com>
2023-04-01 15:44:09 -0700

    chore: commit save point

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/sjtommylee/go-dynamodb/config"
+	"github.com/sjtommylee/go-dynamodb/internal/repository/adapter"
 	"github.com/sjtommylee/go-dynamodb/internal/repository/instance"
+	"github.com/sjtommylee/go-dynamodb/internal/routes"
+	"github.com/sjtommylee/go-dynamodb/utils/logger"
 )
 
 func main() {
 	configs := config.GetConfig()
 	connection := instance.GetConnection()
+	repository := adapter.NewAdapter(connection)
+
+	logger.INFO("service starting....", nil)
+	errors := Migrate(connection)
+	if len(errors) > 0 {
+		for _, err := range errors {
+			logger.PANIC("Error on migrations", err)
+		}
+	}
+	logger.PANIC("", checkTables(connection))
+	port := fmt.Sprintf(":%v", configs.Port)
+	router := routes.NewRouter().SetRouters(repository)
+	logger.INFO("service is running on port", port)
+	server := http.ListenAndServe(port, router)
+	log.Fatal(server)
+
 }
 
-func Migrate() {}
+func Migrate(connection *dynamodb.DynamoDB) []error {
+	var errors []error
 
-func checkTables() {}
+	return errors
+}
+
+func checkTables(connection *dynamodb.DynamoDB) error {
+	// response, err := conenction.ListTables()
+	return nil
+}
--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -2,6 +2,10 @@ package logger
 
 import "log"
 
+func INFO(message string, data interface{}) {
+	log.Printf(message, data)
+}
+
 func PANIC(message string, err error) {
 	if err != nil {
 		log.Panic(message, err)
