113b370fd95ab676484e385f0fa20dbe8ca3994b
sjtommylee <sjtommylee@gmail.com>
2023-08-03 20:27:03 -0700

    db conn, db disconnected

--- account/db/db.go
+++ account/db/db.go
@@ -2,8 +2,12 @@ package db
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 type DB struct {
@@ -13,6 +17,27 @@ type DB struct {
 	db      *mongo.Database
 }
 
-func Connect() {}
+func Connect(ctx context.Context, address string) *DB {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(address))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("connected to the database: %s\n", address)
+	return &DB{
+		context: ctx,
+		address: address,
+		client:  client,
+	}
+}
 
-func Disconnect() {}
+func (d *DB) Disconnect() {
+	if err := d.client.Disconnect(d.context); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Disconnected from the database: %s\n", d.address)
+
+}
