3c93c7a0981aa2d72cc30b2e15259211f16cfa9c
sjtommylee <sjtommylee@gmail.com>
2023-08-03 22:59:27 -0700

    wip

--- account/main/main.go
+++ account/main/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"account/db"
+	pb "account/generated"
 	"account/server"
 	"context"
 	"fmt"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var mongo_host = ""
@@ -16,6 +19,17 @@ var server_adddress = ""
 func main() {
 	db := db.Connect(context.Background(), fmt.Sprintf("mongodb://%s:%s", mongo_host, mongo_port))
 	defer db.Disconnect()
+
+	accountRepository := account.NewRepository(db.Collection("HOST NAME", "HOST PORT"))
+	accountUseCase := account.NewUseCase(accountRepository)
+	accountInterceptor := server.NewInterceptor("")
+	authInterceptor := server.NewInterceptor("Authorization", func(ctx context.Context, header string) error {
+		if header != cfg.ApiKey {
+			return status.Errorf(codes.Unauthenticated, "Invalid API key")
+		}
+		return nil
+	})
+
 	grpcServer := server.Server{Address: server_adddress}
 	grpcServer.Launch(func(server *grpc.Server) {
 		pb.RegisterAccountServiceServer(server, accountService)
