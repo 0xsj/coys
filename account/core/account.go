4cf0d2eebe979a443f12d335b780bbb85757ed94
sjtommylee <sjtommylee@gmail.com>
2023-08-05 11:38:44 -0700

    clean up round 2

--- account/core/account.go
+++
@@ -1,9 +0,0 @@
-package core
-
-type Account struct {
-	Id          string `bson:"_id"`
-	PhoneNumber string `bson:"phoneNumber"`
-	Role        Role   `bson:"role,omitempty"`
-	Status      Status `bson:"status,omitempty"`
-	CreatedAt   int64  `bson:"createdAt,omitempty"`
-}
--- account/fn/fn.go
+++
@@ -1,8 +0,0 @@
-package fn
-
-func Map[T any, R any](in []*T, f func(*T) *R) (out []*R) {
-	for _, i := range in {
-		out = append(out, f(i))
-	}
-	return
-}
