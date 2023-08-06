7b1f0de9826b98caabd9616a2d74b640e88dd415
sjtommylee <sjtommylee@gmail.com>
2023-08-06 13:26:52 -0700

    db connection

--- account/database/database.go
+++ account/database/database.go
@@ -2,8 +2,10 @@ package database
 
 import (
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Database struct {
@@ -12,3 +14,18 @@ type Database struct {
 	client  *mongo.Client
 	db      *mongo.Database
 }
+
+// Connect to db,
+func Connect(ctx context.Context, address string) *Database {
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(address))
+	if err != nil {
+		log.Fatal("Could not connect to the database:", err)
+	}
+
+	return &Database{
+		context: ctx,
+		address: address,
+		client:  client,
+	}
+}
+func Disconnect() {}
