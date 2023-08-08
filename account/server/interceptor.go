61d674e0c323416efd04ba5cee1cc046a9e48269
sjtommylee <sjtommylee@gmail.com>
2023-08-08 01:29:06 -0700

    chore: commit save point

--- account/server/interceptor.go
+++ account/server/interceptor.go
@@ -1,6 +1,16 @@
 package server
 
-func NewInterceptor() {
+import (
+	"context"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func NewInterceptor(name string, callback func(ctx context.Context, header string) error) grpc.ServerOption {
 	// returns a grpc unary interceptor
-	return
+	return grpc.UnaryInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		md, ok := metadata.FromIncomingContext(ctx)
+		return handler(ctx, req)
+	})
 }
