a544a0ab1a470e46fab2ca8e5d680018a50e4cb1
sjtommylee <sjtommylee@gmail.com>
2023-08-07 17:37:43 -0700

    chore: commit save point

--- account/cmd/main.go
+++ account/cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"account/config"
+	"fmt"
+	"log"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(cfg.ApiKey)
+}
