18c16631cd6a646d7f8c7afeee99a33c6eff2a71
sjtommylee <sjtommylee@gmail.com>
2023-08-10 00:21:29 -0700

    chore: commit save point

--- authentication/account/client.go
+++ authentication/account/client.go
@@ -1,1 +1,17 @@
 package account
+
+import (
+	pb "authentication/generated"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewClient(address string) pb.AccountServiceClient {
+	connection, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pb.NewAccountServiceClient(connection)
+}
---
+++ authentication/token/repository.go
@@ -0,0 +1,27 @@
+package token
+
+import (
+	pb "authentication/generated"
+	"context"
+)
+
+type Repository interface {
+	GenerateToken(ctx context.Context, id string) (*Pair, error)
+}
+
+type RepositoryImpl struct {
+	client pb.TokenServiceClient
+}
+
+func NewRepository(client pb.TokenServiceClient) Repository {
+	return &RepositoryImpl{client: client}
+}
+
+func (r *RepositoryImpl) GenerateToken(ctx context.Context, id string) (*Pair, error) {
+	response, err := r.client.GenerateToken(ctx, &pb.GenerateTokenRequest{Id: id})
+	if err != nil {
+		return nil, err
+	}
+
+	return &Pair{AccessToken: response.GetAccessToken(), RefreshToken: response.GetRefreshToken()}, err
+}
