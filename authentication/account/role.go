ce1b2f73a3bd815717a3ce803746e063de00d69d
sjtommylee <sjtommylee@gmail.com>
2023-08-10 00:06:41 -0700

    chore: commit save point

--- authentication/account/role.go
+++ authentication/account/role.go
@@ -1,1 +1,14 @@
 package account
+
+type Role int
+
+const (
+	Admin Role = iota
+	Creator
+	User
+)
+
+// @[r] - we can access an element from the sliced string
+func (r Role) String() string {
+	return [...]string{"ADMIN", "CREATOR", "USER"}[r]
+}
