fc28bb5861ae053c6e09d821406c497b4c2f1e30
sjtommylee <sjtommylee@gmail.com>
2023-08-15 21:42:59 -0700

    chore: commit save point

--- client/src/lib/proto/authentication/SignOutResponse.ts
+++
@@ -1,8 +0,0 @@
-// Original file: ../proto/authentication.proto
-
-
-export interface SignOutResponse {
-}
-
-export interface SignOutResponse__Output {
-}
--- client/src/lib/server/grpc.ts
+++
@@ -1,8 +0,0 @@
-import protoloader from '@grpc/proto-loader';
-import { credentials, loadPackageDefinition } from '@grpc/grpc-js';
-
-const accountPackageDefinition = protoloader.loadSync('./src/lib/proto/account.proto', {});
-
-const accountProto = loadPackageDefinition(accountPackageDefinition);
-
-const accountClient = new accountProto.proto.AccountService();
