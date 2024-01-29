54c667977d2d6c52bdecad3df15bcbda50907f64
sjtommylee <sjtommylee@gmail.com>
2024-01-29 15:30:26 -0800

    chore: commit save point

--- http-rest/main.go
+++ http-rest/main.go
@@ -3,15 +3,38 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
 func main() {
+
+	env, exists := os.LookupEnv("PORT")
+	if !exists {
+	}
+
+	fmt.Println(env)
+
 	port := 3000
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, World!\n")
 	})
 
 	fmt.Println("Server is running on port", port)
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		fmt.Println("Error:", err)
